2650e8464dc7a506b1c5ee8176f00d9d4b3c2707
BrollyMewengkang <brollykmewengkang@gmail.com>
2024-07-14 21:25:23 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    changeSlide(1);
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    changeSlide(-1);
+});
+
+function changeSlide(direction) {
+    slides[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+    slides[currentIndex].classList.add('active');
+}
+
+
+slides[currentIndex].classList.add('active');
+
+
+
+
+
